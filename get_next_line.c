55da210c1b9781bc3a07bbf1059aeb15aa3a8003
Wellerson <wtavares@student.42sp.org.br>
2025-12-18 18:25:42 -0300

    first lines

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,22 @@
 /*   By: wtavares <wtavares@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 17:47:07 by wtavares          #+#    #+#             */
-/*   Updated: 2025/12/18 17:47:08 by wtavares         ###   ########.fr       */
+/*   Updated: 2025/12/18 18:25:12 by wtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	// TODO
+}
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (*str)
+		i++;
+	return	(i);
+}
---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wtavares <wtavares@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 18:02:21 by wtavares          #+#    #+#             */
+/*   Updated: 2025/12/18 18:05:44 by wtavares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+
+#endif
